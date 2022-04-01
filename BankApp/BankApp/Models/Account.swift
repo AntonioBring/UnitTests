33c070f479f107609d40f7f9f6e20a32b1ec9052
antonio.sobrinho <antonio.sobrinho@bringglobal.com>
2022-04-01 17:55:31 +0100

    First Exercise

---
+++ BankApp/BankApp/Models/Account.swift
@@ -0,0 +1,31 @@
+//
+//  Account.swift
+//  BankApp
+//
+//  Created by Mohammad Azam on 3/29/20.
+//  Copyright © 2020 Mohammad Azam. All rights reserved.
+//
+
+import Foundation
+
+enum AccountError: Error{
+    case insufficientFunds
+}
+
+struct Account {
+    var balance: Double = 0.0
+    
+    mutating func deposit(_ amount: Double){
+        self.balance += amount
+    }
+    
+    mutating func withdraw(_ amount: Double) throws{
+        let netBalance = self.balance - amount
+        if netBalance < 0 {
+            throw AccountError.insufficientFunds
+        }else {
+            self.balance -= amount
+        }
+        
+    }
+}
---
+++ BankApp/BankAppTests/BankAppTests.swift
@@ -0,0 +1,44 @@
+//
+//  BankAppTests.swift
+//  BankAppTests
+//
+//  Created by Mohammad Azam on 3/29/20.
+//  Copyright © 2020 Mohammad Azam. All rights reserved.
+//
+
+import XCTest
+@testable import BankApp
+
+class BankAppTests: XCTestCase {
+
+    private var account : Account!
+    
+    override func setUp() {
+        super.setUp()
+        
+        self.account = Account()
+    }
+    
+    func test_InitialBalanceZero() {
+        XCTAssertTrue(self.account.balance == 0, "Balance is not zero!")
+    }
+
+    func test_DepositFunds(){
+        self.account.deposit(100)
+        XCTAssertEqual(100, self.account.balance)
+    }
+    
+    func test_WithdrawFunds() {
+        self.account.deposit(100)
+        try? self.account.withdraw(50)
+        XCTAssertEqual(50, self.account.balance)
+    }
+    
+    func test_WithdrawFundsFromInsufficientBalance(){
+        self.account.deposit(100)
+        
+        XCTAssertThrowsError(try self.account.withdraw(300)) { error in
+            XCTAssertEqual(error as! AccountError, AccountError.insufficientFunds)
+        }
+    }
+}
